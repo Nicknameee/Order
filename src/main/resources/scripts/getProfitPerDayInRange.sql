8e714fbb548596bcf7485920597fcd1e08cacb59
Nicknameee <igrunka1991@gmail.com>
2024-05-11 02:02:47 +0300

    Meow Meow Meow :3

--- src/main/resources/scripts/getProfitPerDayInRange.sql
+++ src/main/resources/scripts/getProfitPerDayInRange.sql
@@ -1,6 +1,18 @@
-SELECT sale.time, SUM(sale.total_cost * (product.margin_rate - 1)) as ovle
-FROM product_sales_statistic sale
-INNER JOIN products product ON product.product_id = sale.product_id
-WHERE sale.time BETWEEN ? AND ?
-GROUP BY sale.time
-ORDER BY sale.time DESC
+SELECT
+    sale.time,
+    SUM(
+            CASE
+                WHEN product.currency = 'UAH' THEN sale.total_cost * (product.margin_rate - 1) / 39
+                ELSE sale.total_cost * (product.margin_rate - 1)
+                END
+        ) as ovle
+FROM
+    product_sales_statistic sale
+        INNER JOIN
+    products product ON product.product_id = sale.product_id
+WHERE
+    sale.time BETWEEN ? AND ?
+GROUP BY
+    sale.time
+ORDER BY
+    sale.time DESC;
